adff33a0518db7d9e6b63644abbbf7c3ed8bfb7c
LudSkop <luda.skopenko80@gmail.com>
2024-07-30 12:48:26 +0300

    added list_phones

--- Class/string.py
+++ Class/string.py
@@ -7,13 +7,43 @@ class Phones(UserList):
             new_phone = "+" + phone
         elif len(phone) < 12:
             new_phone = "+38" + phone
+        else:
+            raise
         self.data.append(new_phone)
 
     def get_phones(self):
         return self.data
 
 
-phone_list = Phones()
-phone_list.set_phone('0963610573')
-phone_list.set_phone('380963610573')
-print(phone_list.get_phones)
+#phone_list = Phones()
+#phone_list.set_phone('0963610573')
+#phone_list.set_phone('380963610573')
+#print(phone_list.get_phones)
+
+
+class User(UserDict):
+    def set_name(self, name):
+        self.data['name'] = name
+
+    def get_name(self):
+        return self.data.get('name')
+
+    def set_phone(self, phone):
+        phone_list = self.data.get('phones', Phones())
+        phone_list.data.append(phone)
+        self.data['phones'] = phone_list
+
+    def get_phones(self):
+        return self.data.get('phones')
+
+
+user_1 = User()
+user_1.set_name('Alisa')
+user_1.set_phone('380983864330')
+user_1.set_phone('0963610573')
+user_1.set_phone('380637362574')
+print(user_1.get_phones())
+print(user_1.get_name(), user_1.get_phones())
+
+
+
