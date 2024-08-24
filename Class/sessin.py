98fbcf06740b52406b3740fd3efe88d30fba943c
LudSkop <luda.skopenko80@gmail.com>
2024-08-24 21:18:17 +0300

    added sesin

---
+++ Class/sessin.py
@@ -0,0 +1,7 @@
+file_name = "expenses.txt"
+expenses = {}
+with open(file_name, "r") as fh:
+    raw_expenses = fh.readlines()
+    for line in raw_expenses:
+        key, value = line.split("|")
+        expenses[key] = int(value)
