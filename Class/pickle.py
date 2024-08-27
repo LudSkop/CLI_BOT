21e480c49f6d1af7267f05ddf242581e79fab3a8
LudSkop <luda.skopenko80@gmail.com>
2024-08-27 23:02:29 +0300

    added pickle

---
+++ Class/pickle.py
@@ -0,0 +1,12 @@
+import pickle
+
+
+def write_contacts_to_file(filename, contacts):
+    with open(filename, 'wb') as file:
+        pickle.dump(contacts, file)
+
+
+def read_contacts_from_file(filename):
+    with open(filename, 'rb') as file:
+        return pickle.load(file)
+
