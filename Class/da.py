a278d46c7170ed6d84bbaa0adf1041a3f0ec88f9
LudSkop <luda.skopenko80@gmail.com>
2024-09-02 23:14:19 +0300

    added deepcopy

--- Class/da.py
+++ Class/da.py
@@ -7,3 +7,9 @@ l_copy = copy(l)
 l_deepcopy = deepcopy(l)
 print(l == list_l)
 print(l == l_copy, l == l_deepcopy)
+
+l[0] = 9
+print(l, list_l, l_copy, l_deepcopy)
+
+l[3][0] = 'Hello'
+print(l, list_l, l_copy, l_deepcopy)
